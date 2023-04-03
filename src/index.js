02088fd9c27af4b7ff6a8b2ce7b48681375d4139
Nhat Loi <nhatloi120402@gmail.com>
2023-04-03 21:15:13 +0700

    add login profile register detailPost changePass page

--- src/index.js
+++ src/index.js
@@ -16,6 +16,30 @@ app.get('/', function (req, res) {
     return res.render('home', { layout: 'main' });
 });
 
+app.get('/login', function (req, res) {
+    return res.render('login', { layout: 'main' });
+});
+
+app.get('/register', function (req, res) {
+    return res.render('register', { layout: 'main' });
+});
+
+app.get('/admin', function (req, res) {
+    return res.render('admin', { layout: 'main' });
+});
+
+app.get('/detailPost', function (req, res) {
+    return res.render('detailPost', { layout: 'main' });
+});
+
+app.get('/profile', function (req, res) {
+    return res.render('profile', { layout: 'main' });
+});
+
+app.get('/changePass', function (req, res) {
+    return res.render('changePass', { layout: 'main' });
+});
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
