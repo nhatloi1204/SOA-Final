d51959999698710a006affa034f4deb60e1993ab
Nhat Loi <nhatloi120402@gmail.com>
2023-04-23 04:14:57 +0700

    login admin, dashboard

--- src/resources/routes/adminRoutes.js
+++ src/resources/routes/adminRoutes.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 const { requireAuth } = require('../middleware/authenticatedCheck');
 
 router.get('/', function (req, res) {
-    res.json('welcome to admin page');
+    res.render('admin', { layout: 'admin' });
+});
+
+router.get('/dashboard', function (req, res) {
+    res.render('dashboard', { layout: 'admin' });
 });
 
 module.exports = router;
