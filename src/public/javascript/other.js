496f70dd194505c85a4e1ae3d4a8cdf0031623c9
Nhat Loi <nhatloi120402@gmail.com>
2023-04-26 00:13:31 +0700

    truncate postCard, add selection tags and css input file in newStoryPage, updated dashboard and add Posts,Users,Staff,Tags page, add Following and Bookmarks page

--- src/public/javascript/other.js
+++ src/public/javascript/other.js
@@ -6,3 +6,26 @@ export function uploadAvatar() {
         image.src = URL.createObjectURL(input.files[0]);
     });
 }
+
+// Get the button
+let myButton = document.getElementById('btn-back-to-top');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+export function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myButton.style.display = 'block';
+    } else {
+        myButton.style.display = 'none';
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+myButton.addEventListener('click', backToTop);
+
+export function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
